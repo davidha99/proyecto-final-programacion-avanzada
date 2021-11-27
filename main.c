167ef5073d0f0dee6ca3535c13421eecb3ba4b7c
David Hernández <d.hdza19@gmail.com>
2021-11-27 03:17:42 -0600

    Fix algoritmo substring

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,23 +36,23 @@ void algoritmo_substring() {
     /*Thread 2: 101-200*/
     /*Thread 3: 201-300*/
     FILE *archivo_secuencia;
-    char *secuencia;
-    size_t len = 0;
-    ssize_t read;
-    archivo_secuencia = fopen("S._cerevisiae_2021_03.seq", "r");
-    // printf("%s", "Estamos aquí");
+    char *secuencia = NULL;
+    size_t secuencia_line_buf = 0;
+    ssize_t secuencia_len;
+    archivo_secuencia = fopen("s_cerevisia_2021_03.seq", "r");
 
-    /*Variables para el Core del algoritmo*/
-    int secuencia_len;
-    int i, j, k, bandera = 0;
+    secuencia_len = getline(&secuencia, &secuencia_line_buf, archivo_secuencia);
+
+    /*Variables para el algoritmo de encontrar substring*/
+    int i, j, k, encontrado = 0;
     posicion pos;
 
-    // /*secuencia ya es un string*/
+    while (secuencia_len >= 0) {
+        /*Le restamos -2 porque por el getline me regresaba dos caracteres de más. Por ejemplo,
+        para la primera secuencia me tomaba que el tamaño era 12,916 cuando en realidad era 12,914*/
+        secuencia_len -= 2;
 
-    while (fgets(secuencia, sizeof(secuencia), archivo_secuencia) != NULL) {
-        // printf("%s", secuencia);
-        /*Core del algoritmo*/
-        secuencia_len = strlen(secuencia);
+        /*Buscar si la secuencia está en la referencia*/
         for (i = 0; referencia[i]; i++) {
             k = i;
             for (j = 0; j <= secuencia_len - 1; j++) {
@@ -63,19 +64,26 @@ void algoritmo_substring() {
                 pos.pos_ini = i;
                 pos.pos_fin = k - 1;
                 secuencias[cont_secuencias] = pos;
-                bandera = 1;
+                encontrado = 1;
             }
 
-            if (bandera == 1)
+            if (encontrado == 1)
                 break;
         }
 
-        if (bandera == 0) {
+        if (encontrado == 0) {
             pos.pos_ini = -1;
             pos.pos_fin = -1;
             secuencias[cont_secuencias] = pos;
         }
+        /*Aumentamos contador para guardar la siguiente estructura de tipo "posición"*/
         cont_secuencias++;
+
+        /*Reseteamos variable encontrado para la próxima secuencia*/
+        encontrado = 0;
+
+        /*Get the next line*/
+        secuencia_len = getline(&secuencia, &secuencia_line_buf, archivo_secuencia);
     }
     fclose(archivo_secuencia);
 }
@@ -88,19 +96,11 @@ int main() {
 
     obtener_string_referencia();
 
-    // for (int s = 1; s < 1001; s++) {
-    //     secuencias[s] = algoritmo_substring();
-    // }
-
     algoritmo_substring();
 
     for (int i = 1; i < 1001; i++) {
         printf("Sec %d: Posición inicial: %d, Posición final: %d\n", i, secuencias[i].pos_ini, secuencias[i].pos_fin);
     }
-
-    // for (int i = secuencias[1].pos_ini; i <= secuencias[1].pos_fin; i++) {
-    //     printf("%c", referencia[i]);
-    // }
 
     // Thread 1: 1-100
     // for (int i = 1; i <= 100; i++) {
